617f88f6687b307161c21ac913d7435403c4ad1e
jimzrt <james.tophoven@gmail.com>
2017-07-24 10:09:57 +0200

    refactor www.js

--- app.js
+++ app.js
@@ -10,6 +10,9 @@ var bodyParser = require('body-parser');
 
 var app = express();
 
+var port = normalizePort(process.env.PORT || '3000');
+app.set('port', port);
+
 var server = require('http').createServer(app);  
 var io = require('socket.io')(server);
 //var index = require('./routes/index');
@@ -55,8 +58,69 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
-server.listen(5000);  
-
+server.listen(port);
+server.on('error', onError);
+server.on('listening', onListening);
 
 module.exports = app;
+
+
+/**
+ * Normalize a port into a number, string, or false.
+ */
+
+function normalizePort(val) {
+  var port = parseInt(val, 10);
+
+  if (isNaN(port)) {
+    // named pipe
+    return val;
+  }
+
+  if (port >= 0) {
+    // port number
+    return port;
+  }
+
+  return false;
+}
+
+/**
+ * Event listener for HTTP server "error" event.
+ */
+
+function onError(error) {
+  if (error.syscall !== 'listen') {
+    throw error;
+  }
+
+  var bind = typeof port === 'string'
+    ? 'Pipe ' + port
+    : 'Port ' + port;
+
+  // handle specific listen errors with friendly messages
+  switch (error.code) {
+    case 'EACCES':
+      console.error(bind + ' requires elevated privileges');
+      process.exit(1);
+      break;
+    case 'EADDRINUSE':
+      console.error(bind + ' is already in use');
+      process.exit(1);
+      break;
+    default:
+      throw error;
+  }
+}
+
+/**
+ * Event listener for HTTP server "listening" event.
+ */
+
+function onListening() {
+  var addr = server.address();
+  var bind = typeof addr === 'string'
+    ? 'pipe ' + addr
+    : 'port ' + addr.port;
+}
 
